9b4b8355624291ec80de1887b74ec64cfa7a1148
Montu Meena <158208396+montumeena141@users.noreply.github.com>
2025-02-14 12:47:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+    let price=document.querySelectorAll(".price");
+	let sum=0;
+
+	price.forEach((price)=>{
+		sum+=parseInt(price.textContent);
+	})
+
+	let newRowExist=document.getElementById("ans");
+
+	if(!newRowExist){
+	let newRow=document.createElement("tr");
+	newRow.id="ans";
+	let newCell=document.createElement("td");
+	newCell.textContent=`Total Price = ${sum}`;
+
+	newRow.append(newCell);
+
+	document.querySelector("table").append(newRow);
+	}
 };
 
 getSumBtn.addEventListener("click", getSum);
